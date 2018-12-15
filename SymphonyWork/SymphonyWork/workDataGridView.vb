b75f1a3497b91aba5346973f2bc15a3c86e18797
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2018-12-15 12:34:29 +0900

    勤務表：作り直し開始

---
+++ SymphonyWork/SymphonyWork/workDataGridView.vb
@@ -0,0 +1,19 @@
+﻿Public Class workDataGridView
+    Inherits DataGridView
+
+    Private Sub ExDataGridView_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles Me.CellPainting
+        '選択したセルに枠を付ける
+        If e.ColumnIndex >= 0 AndAlso e.RowIndex >= 0 AndAlso (e.PaintParts And DataGridViewPaintParts.Background) = DataGridViewPaintParts.Background Then
+            e.Graphics.FillRectangle(New SolidBrush(e.CellStyle.BackColor), e.CellBounds)
+
+            If (e.PaintParts And DataGridViewPaintParts.SelectionBackground) = DataGridViewPaintParts.SelectionBackground AndAlso (e.State And DataGridViewElementStates.Selected) = DataGridViewElementStates.Selected Then
+                e.Graphics.DrawRectangle(New Pen(Color.Black, 2I), e.CellBounds.X + 1I, e.CellBounds.Y + 1I, e.CellBounds.Width - 3I, e.CellBounds.Height - 3I)
+            End If
+
+            Dim pParts As DataGridViewPaintParts = e.PaintParts And Not DataGridViewPaintParts.Background
+            e.Paint(e.ClipBounds, pParts)
+            e.Handled = True
+        End If
+    End Sub
+
+End Class
--- SymphonyWork/SymphonyWork/勤務割.Designer.vb
+++ SymphonyWork/SymphonyWork/勤務割.Designer.vb
@@ -22,7 +22,7 @@ Partial Class 勤務割
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.dgvWork = New System.Windows.Forms.DataGridView()
+        Me.components = New System.ComponentModel.Container()
         Me.rbtn3F = New System.Windows.Forms.RadioButton()
         Me.rbtn2F = New System.Windows.Forms.RadioButton()
         Me.btnRowAdd = New System.Windows.Forms.Button()
@@ -30,22 +30,36 @@ Partial Class 勤務割
         Me.btnRegist = New System.Windows.Forms.Button()
         Me.btnPrint = New System.Windows.Forms.Button()
         Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.ymBox = New ymdBox.ymdBox()
+        Me.dgvWork = New SymphonyWork.workDataGridView(Me.components)
         CType(Me.dgvWork, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'dgvWork
-        '
-        Me.dgvWork.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvWork.Location = New System.Drawing.Point(50, 91)
-        Me.dgvWork.Name = "dgvWork"
-        Me.dgvWork.RowTemplate.Height = 21
-        Me.dgvWork.Size = New System.Drawing.Size(977, 630)
-        Me.dgvWork.TabIndex = 0
         '
         'rbtn3F
         '
         Me.rbtn3F.AutoSize = True
-        Me.rbtn3F.Location = New System.Drawing.Point(211, 23)
+        Me.rbtn3F.Location = New System.Drawing.Point(152, 14)
         Me.rbtn3F.Name = "rbtn3F"
         Me.rbtn3F.Size = New System.Drawing.Size(43, 16)
         Me.rbtn3F.TabIndex = 1
@@ -56,7 +70,7 @@ Partial Class 勤務割
         '
         Me.rbtn2F.AutoSize = True
         Me.rbtn2F.Checked = True
-        Me.rbtn2F.Location = New System.Drawing.Point(211, 50)
+        Me.rbtn2F.Location = New System.Drawing.Point(152, 41)
         Me.rbtn2F.Name = "rbtn2F"
         Me.rbtn2F.Size = New System.Drawing.Size(43, 16)
         Me.rbtn2F.TabIndex = 2
@@ -66,7 +80,7 @@ Partial Class 勤務割
         '
         'btnRowAdd
         '
-        Me.btnRowAdd.Location = New System.Drawing.Point(402, 40)
+        Me.btnRowAdd.Location = New System.Drawing.Point(318, 31)
         Me.btnRowAdd.Name = "btnRowAdd"
         Me.btnRowAdd.Size = New System.Drawing.Size(55, 23)
         Me.btnRowAdd.TabIndex = 3
@@ -75,7 +89,7 @@ Partial Class 勤務割
         '
         'btnRowDelete
         '
-        Me.btnRowDelete.Location = New System.Drawing.Point(463, 40)
+        Me.btnRowDelete.Location = New System.Drawing.Point(379, 31)
         Me.btnRowDelete.Name = "btnRowDelete"
         Me.btnRowDelete.Size = New System.Drawing.Size(55, 23)
         Me.btnRowDelete.TabIndex = 4
@@ -84,36 +98,291 @@ Partial Class 勤務割
         '
         'btnRegist
         '
-        Me.btnRegist.Location = New System.Drawing.Point(543, 23)
+        Me.btnRegist.Location = New System.Drawing.Point(459, 20)
         Me.btnRegist.Name = "btnRegist"
-        Me.btnRegist.Size = New System.Drawing.Size(80, 40)
+        Me.btnRegist.Size = New System.Drawing.Size(80, 35)
         Me.btnRegist.TabIndex = 5
         Me.btnRegist.Text = "登録"
         Me.btnRegist.UseVisualStyleBackColor = True
         '
         'btnPrint
         '
-        Me.btnPrint.Location = New System.Drawing.Point(715, 23)
+        Me.btnPrint.Location = New System.Drawing.Point(631, 20)
         Me.btnPrint.Name = "btnPrint"
-        Me.btnPrint.Size = New System.Drawing.Size(80, 40)
+        Me.btnPrint.Size = New System.Drawing.Size(80, 35)
         Me.btnPrint.TabIndex = 6
         Me.btnPrint.Text = "印刷"
         Me.btnPrint.UseVisualStyleBackColor = True
         '
         'btnDelete
         '
-        Me.btnDelete.Location = New System.Drawing.Point(629, 23)
+        Me.btnDelete.Location = New System.Drawing.Point(545, 20)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(80, 40)
+        Me.btnDelete.Size = New System.Drawing.Size(80, 35)
         Me.btnDelete.TabIndex = 7
         Me.btnDelete.Text = "削除"
         Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(47, 669)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 12)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "0 ： ｸﾘｱ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(100, 669)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 12)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "1 ： 早出"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Blue
+        Me.Label3.Location = New System.Drawing.Point(218, 669)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 12)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "3 ： 日勤"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Blue
+        Me.Label4.Location = New System.Drawing.Point(160, 669)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 12)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "2 ： 日早"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Blue
+        Me.Label5.Location = New System.Drawing.Point(394, 669)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 12)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "6 ： 遅々"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.Blue
+        Me.Label6.Location = New System.Drawing.Point(336, 669)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 12)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "5 ： 遅出"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.Blue
+        Me.Label7.Location = New System.Drawing.Point(276, 669)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 12)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "4 ： 日遅"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.Blue
+        Me.Label8.Location = New System.Drawing.Point(507, 669)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(45, 12)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "8： 深夜"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.Color.Blue
+        Me.Label9.Location = New System.Drawing.Point(449, 669)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 12)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "7 ： 夜勤"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.ForeColor = System.Drawing.Color.Blue
+        Me.Label11.Location = New System.Drawing.Point(907, 692)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(55, 12)
+        Me.Label11.TabIndex = 24
+        Me.Label11.Text = "36 ： 特休"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.ForeColor = System.Drawing.Color.Blue
+        Me.Label12.Location = New System.Drawing.Point(847, 669)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(55, 12)
+        Me.Label12.TabIndex = 23
+        Me.Label12.Text = "22 ： 研修"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.ForeColor = System.Drawing.Color.Blue
+        Me.Label13.Location = New System.Drawing.Point(788, 669)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(55, 12)
+        Me.Label13.TabIndex = 22
+        Me.Label13.Text = "21 ： 半行"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.ForeColor = System.Drawing.Color.Blue
+        Me.Label14.Location = New System.Drawing.Point(729, 669)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(55, 12)
+        Me.Label14.TabIndex = 21
+        Me.Label14.Text = "13 ： 半夜"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.ForeColor = System.Drawing.Color.Blue
+        Me.Label15.Location = New System.Drawing.Point(675, 669)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(51, 12)
+        Me.Label15.TabIndex = 20
+        Me.Label15.Text = "12 ： 半B"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.ForeColor = System.Drawing.Color.Blue
+        Me.Label16.Location = New System.Drawing.Point(624, 669)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(47, 12)
+        Me.Label16.TabIndex = 19
+        Me.Label16.Text = "11： 半A"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.ForeColor = System.Drawing.Color.Blue
+        Me.Label17.Location = New System.Drawing.Point(568, 669)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(43, 12)
+        Me.Label17.TabIndex = 18
+        Me.Label17.Text = "10 ： 半"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.ForeColor = System.Drawing.Color.Blue
+        Me.Label10.Location = New System.Drawing.Point(847, 692)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(55, 12)
+        Me.Label10.TabIndex = 30
+        Me.Label10.Text = "35 ： 産休"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.ForeColor = System.Drawing.Color.Blue
+        Me.Label18.Location = New System.Drawing.Point(788, 692)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(55, 12)
+        Me.Label18.TabIndex = 29
+        Me.Label18.Text = "34 ： 希休"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.ForeColor = System.Drawing.Color.Blue
+        Me.Label20.Location = New System.Drawing.Point(675, 692)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(53, 12)
+        Me.Label20.TabIndex = 27
+        Me.Label20.Text = "33 ： 明け"
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.ForeColor = System.Drawing.Color.Blue
+        Me.Label21.Location = New System.Drawing.Point(624, 692)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(51, 12)
+        Me.Label21.TabIndex = 26
+        Me.Label21.Text = "32： 公休"
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.ForeColor = System.Drawing.Color.Blue
+        Me.Label22.Location = New System.Drawing.Point(568, 692)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(55, 12)
+        Me.Label22.TabIndex = 25
+        Me.Label22.Text = "31 ： 有休"
+        '
+        'ymBox
+        '
+        Me.ymBox.boxType = 5
+        Me.ymBox.DateText = ""
+        Me.ymBox.EraLabelText = "H30"
+        Me.ymBox.EraText = ""
+        Me.ymBox.Location = New System.Drawing.Point(41, 14)
+        Me.ymBox.MonthLabelText = "12"
+        Me.ymBox.MonthText = ""
+        Me.ymBox.Name = "ymBox"
+        Me.ymBox.Size = New System.Drawing.Size(95, 40)
+        Me.ymBox.TabIndex = 31
+        '
+        'dgvWork
+        '
+        Me.dgvWork.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvWork.Location = New System.Drawing.Point(36, 74)
+        Me.dgvWork.Name = "dgvWork"
+        Me.dgvWork.RowTemplate.Height = 21
+        Me.dgvWork.Size = New System.Drawing.Size(925, 586)
+        Me.dgvWork.TabIndex = 8
         '
         '勤務割
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1189, 747)
+        Me.ClientSize = New System.Drawing.Size(997, 722)
+        Me.Controls.Add(Me.ymBox)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvWork)
         Me.Controls.Add(Me.btnDelete)
         Me.Controls.Add(Me.btnPrint)
         Me.Controls.Add(Me.btnRegist)
@@ -121,7 +390,6 @@ Partial Class 勤務割
         Me.Controls.Add(Me.btnRowAdd)
         Me.Controls.Add(Me.rbtn2F)
         Me.Controls.Add(Me.rbtn3F)
-        Me.Controls.Add(Me.dgvWork)
         Me.Name = "勤務割"
         Me.Text = "workForm"
         CType(Me.dgvWork, System.ComponentModel.ISupportInitialize).EndInit()
@@ -130,7 +398,6 @@ Partial Class 勤務割
 
     End Sub
 
-    Friend WithEvents dgvWork As DataGridView
     Friend WithEvents rbtn3F As RadioButton
     Friend WithEvents rbtn2F As RadioButton
     Friend WithEvents btnRowAdd As Button
@@ -138,4 +405,27 @@ Partial Class 勤務割
     Friend WithEvents btnRegist As Button
     Friend WithEvents btnPrint As Button
     Friend WithEvents btnDelete As Button
+    Friend WithEvents dgvWork As SymphonyWork.workDataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label18 As System.Windows.Forms.Label
+    Friend WithEvents Label20 As System.Windows.Forms.Label
+    Friend WithEvents Label21 As System.Windows.Forms.Label
+    Friend WithEvents Label22 As System.Windows.Forms.Label
+    Friend WithEvents ymBox As ymdBox.ymdBox
 End Class
